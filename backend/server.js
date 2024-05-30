6db14151d044bd162e4d308c790f6fb9a51e00a0
Hyde <audrinahasibuan@gmail.com>
2024-05-30 23:04:29 +0700

    skibidi toilet

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors());
 
 connector.connectDB();
 app.use(route);
