a6e2608f205598e86e6a2941609698cd118a17f1
marsyuma <bintang.marsyuma@ui.ac.id>
2024-05-31 21:08:53 +0700

    Sample Backend kostCont.js

--- backend/route.js
+++ backend/route.js
@@ -1,8 +1,14 @@
 const userCont = require ("./controller/userCont.js");
+const kostController = require("./controller/kostCont.js");
 const express = require ("express");
 const router = express.Router();
 
 router.post("/login", userCont.login);
 router.post("/signUp", userCont.signUp);
+// Route for adding a new kost
+router.post("/kost/addkost", kostController.addKost);
+
+// Route for editing an existing kost
+router.put("/profile/mykost/editkost/:id", kostController.editKost);
 
 module.exports = router;
